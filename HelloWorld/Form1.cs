eb5f744ba34b89b3d89dd98ebda37b9858fef1e0
Ruveydabal <ruveydabal024@gmail.com>
2025-02-26 08:37:57 +0100

    added class Card, added enum's in the class

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -1,10 +1,16 @@
+using BlackJack.classes;
+
 namespace BlackJack
 {
     public partial class Form1 : Form
     {
+        Card twoOfHearts = new Card(Suits.HEARTS, FaceValues.TWO);
         public Form1()
         {
             InitializeComponent();
+
+            Console.WriteLine(twoOfHearts.Suits);
+
         }
 
         private void button1_Click(object sender, EventArgs e)
---
+++ HelloWorld/classes/Card.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.classes
+{
+    public enum Suits
+    {
+        CLUBS,
+        DIAMONDS,
+        HEARTS,
+        SPADES
+    }
+
+    public enum FaceValues
+    {
+        ACE = 1, 
+        TWO, 
+        THREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE,
+        TEN,
+        JACK,
+        QUEEN,
+        KING
+    }
+
+     class Card
+    {
+        Suits suits;
+        FaceValues faceValues;
+        int value;
+        Image img; 
+
+        public Suits Suits 
+        {  
+            get 
+            { 
+                return suits; 
+            } 
+            set
+            {
+                suits = value;
+            }
+        }
+
+
+        public Card(Suits suits, FaceValues faceValues)
+        {
+            this.suits = suits;
+            this.faceValues = new FaceValues();
+
+            switch(faceValues)
+            {
+                case FaceValues.ACE:
+                    this.value = 11;
+                    break;
+                    case FaceValues.TEN:
+                    case FaceValues.JACK:
+                    case FaceValues.QUEEN:
+                    case FaceValues.KING:
+                        this.value = 10;
+                        break;
+                    default:
+                        this.value = (int)faceValues;
+                        break;
+            }
+        }
+
+        public override 
+    }
+}
