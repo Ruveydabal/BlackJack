e5b94ca330926481a619c402dbe2216af82d2435
Ruveydabal <ruveydabal024@gmail.com>
2025-04-16 09:59:03 +0200

    I now have a point sytem. If the dealer clicks on the right button for the right action they will get a point. But if they click on the button that is not the right next action they will get a minus mpoint for every wron choice

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -11,11 +11,11 @@ namespace BlackJack
         Hand hand;
         //List<Card> cards = new List<Card>();
         Player[] players = new Player[2];
-        int points;
 
 
 
-        private enum GameStates
+
+        public enum GameStates
         {//Need to make a state diagram to use this
             //This is the order of how the player has to go about the game.
             SHUFLLE_CARDS,
@@ -25,6 +25,8 @@ namespace BlackJack
             CHECK_NATURAL_DEALER,
         }
 
+        int pointsDealer;
+        GameStates currentGameStates = GameStates.SHUFLLE_CARDS;
         public Form1()
         {//BlackJack game
             InitializeComponent();
@@ -33,24 +35,40 @@ namespace BlackJack
             hand = new Hand(dealerCard1, DealerCard2);
         }
 
-        /*public void HandleEvent()
+        public void HandleEvent(GameStates gameState)
         {
-            if ()
+            if (currentGameStates != gameState)
             {
-
+                pointsDealer--;
             }
             else
             {
-
+                pointsDealer++;
+                switch (gameState)
+                {
+                    case GameStates.SHUFLLE_CARDS:
+                        currentGameStates = GameStates.DEAL_AT_START;
+                        break;
+                    case GameStates.DEAL_AT_START:
+                        currentGameStates = GameStates.CHECK_NATURAL_PLAYER1;
+                        break;
+                    case GameStates.CHECK_NATURAL_PLAYER1:
+                        currentGameStates = GameStates.CHECK_NATURAL_PLAYER2;
+                        break;
+                    case GameStates.CHECK_NATURAL_PLAYER2:
+                        currentGameStates = GameStates.CHECK_NATURAL_DEALER;
+                        break;
+                }
             }
-        }*/
+
+            PointsForDealer.Text = pointsDealer.ToString();
+        }
 
         private void ShuffleDeck_Click(object sender, EventArgs e)
-        {//When you 
-
+        {
+            HandleEvent(GameStates.SHUFLLE_CARDS);
             deck.Shuffle();
             System.Diagnostics.Debug.WriteLine("it works");
-
         }
 
         private void DrawCard_Click(object sender, EventArgs e)
@@ -89,20 +107,43 @@ namespace BlackJack
             Card dealingHiddenCard = DealCards();
             hand.recieveCard(dealingHiddenCard);//second card is hidden
             System.Diagnostics.Debug.WriteLine("You have dealt the cards.");
+            HandleEvent(GameStates.DEAL_AT_START);
 
         }
         public void CheckNatural()
         {//dealer has to check if the hand of the player has a Natural
-            
-            if (players[0].Natural() == true || players[1].Natural() == true)
+
+            if (players[0].Natural() == true)
             {
+                System.Diagnostics.Debug.WriteLine("player has natural");
+            }
+            if (players[1].Natural() == true)
+            {
+                HandleEvent(GameStates.CHECK_NATURAL_PLAYER2);
                 System.Diagnostics.Debug.WriteLine("player has natural");
             }
         }
 
         private void natural_Click(object sender, EventArgs e)
         {
-          CheckNatural();
+            CheckNatural();
+        }
+
+        private void CheckNaturalPlayer1_Click(object sender, EventArgs e)
+        {
+            CheckNatural();
+            HandleEvent(GameStates.CHECK_NATURAL_PLAYER1);
+        }
+
+        private void CheckNaturalPlayer2_Click(object sender, EventArgs e)
+        {
+            CheckNatural();
+            HandleEvent(GameStates.CHECK_NATURAL_PLAYER2);
+        }
+
+        private void CheckNaturalDealer_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
